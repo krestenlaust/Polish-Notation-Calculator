7da6edc118b1a1ee50fae8d5e1ed2b7b7a85e467
Kresten Laust <krestenlaust@gmail.com>
2023-10-02 00:32:33 +0200

    Operator factory

--- Ziglang/src/main.zig
+++ Ziglang/src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const print = @import("std").debug.print;
+const stdin = std.io.getStdIn().reader();
 
 pub fn main() !void {
     var final_value: i32 = calculate();
@@ -25,9 +26,30 @@ pub fn calculate() i32 {
 pub fn query_next_operation() Operation {
     print("Enter operator and an optional operand: ", .{});
     // Get operator.
+
+    var buf: [std.mem.page_size]u8 = undefined;
+    var line = try stdin.readUntilDelimiterOrEof(buf[0..], '\n');
+    var iter = std.mem.split(u8, line, " ");
+
+    var operator_symbol: ?[]u8 = iter.next();
+    _ = operator_symbol;
+
+    var operand: ?[]u8 = iter.next();
+
+    if (operand == null) {}
 }
 
 const Operation = struct {
     operator: u8,
     operand: ?i32,
 };
+
+pub fn unary_factory(operator: u8) ?fn (i32, i32) i32 {
+    _ = operator;
+    // Return function for computing result of unary operator.
+}
+
+pub fn binary_factory(operator: u8) ?fn (i32) i32 {
+    _ = operator;
+    // Return function for computing result of binary operator.
+}
