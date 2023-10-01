b124599114fe8c6d2c8abe3836297eb51c33c3a4
Kresten Laust <krestenlaust@gmail.com>
2023-10-02 00:22:15 +0200

    Calculate and query next operation

--- Ziglang/src/main.zig
+++ Ziglang/src/main.zig
@@ -8,4 +8,26 @@ pub fn main() !void {
 }
 
 pub fn calculate() i32 {
+    var result: i32 = 123;
+
+    while (true) {
+        var op = query_next_operation();
+        _ = op;
+
+        // Do operation.
+
+        print("Result so far: {d}\n", .{result});
+    }
+
+    return result;
 }
+
+pub fn query_next_operation() Operation {
+    print("Enter operator and an optional operand: ", .{});
+    // Get operator.
+}
+
+const Operation = struct {
+    operator: u8,
+    operand: ?i32,
+};
