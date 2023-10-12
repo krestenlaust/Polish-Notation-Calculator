8e78fe923f0d89f22db6e06a0c25f82634ea854a
Kresten Laust <krestenlaust@gmail.com>
2023-10-12 23:58:18 +0200

    Added tests

--- Ziglang/src/main.zig
+++ Ziglang/src/main.zig
@@ -62,3 +62,34 @@ fn query_next_operation() !Operation {
     // Ignore errors as specified in task.
     return try parsing.parse_operation_input(line);
 }
+
+// tests
+test "test do next op example sequence" {
+    comptime var accumulator: f32 = 0;
+    accumulator = comptime do_operation(Operation{ .operator = '+', .operand = 5 }, accumulator) catch unreachable; // Enter operator, and an optional operand: + 5.000000
+    try std.testing.expectEqual(5.0, accumulator); // Result so far is 5.000000.
+
+    accumulator = comptime do_operation(Operation{ .operator = '-', .operand = 6 }, accumulator) catch unreachable; // Enter operator, and an optional operand: - 6.000000
+    try std.testing.expectEqual(-1.0, accumulator); // Result so far is -1.000000.
+
+    accumulator = comptime do_operation(Operation{ .operator = '*', .operand = 3 }, accumulator) catch unreachable; // Enter operator, and an optional operand: * 3.000000
+    try std.testing.expectEqual(-3.0, accumulator); // Result so far is -3.000000.
+
+    accumulator = comptime do_operation(Operation{ .operator = '#', .operand = null }, accumulator) catch unreachable; // Enter operator, and an optional operand: #
+    try std.testing.expectEqual(-3.0, accumulator); // Result so far is -3.000000.
+
+    accumulator = comptime do_operation(Operation{ .operator = '%', .operand = null }, accumulator) catch unreachable; // Enter operator, and an optional operand: %
+    try std.testing.expectEqual(3.0, accumulator); // Result so far is 3.000000.
+
+    accumulator = comptime do_operation(Operation{ .operator = '#', .operand = null }, accumulator) catch unreachable; // Enter operator, and an optional operand: #
+    try std.testing.expect(1.732051 - accumulator < 0.000001); // Result so far is 1.732051.
+
+    accumulator = comptime do_operation(Operation{ .operator = '^', .operand = 2 }, accumulator) catch unreachable; // Enter operator, and an optional operand: ^ 2.000000
+    try std.testing.expectEqual(3.0, accumulator); // Result so far is 3.000000.
+
+    accumulator = comptime do_operation(Operation{ .operator = '*', .operand = 4 }, accumulator) catch unreachable; // Enter operator, and an optional operand: * 4.000000
+    try std.testing.expectEqual(12.0, accumulator); // Result so far is 12.000000.
+
+    accumulator = comptime do_operation(Operation{ .operator = '!', .operand = null }, accumulator) catch unreachable; // Enter operator, and an optional operand: !
+    try std.testing.expect(0.083333 - accumulator < 0.000001); // Result so far is 0.083333.
+}
