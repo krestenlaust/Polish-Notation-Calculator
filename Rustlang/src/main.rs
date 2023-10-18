5c746bf60838057373b3eeb21e56ca4da5c3f5b6
Kresten Laust <krestenlaust@gmail.com>
2023-10-19 00:11:43 +0200

    Worked on 'calculate' function body

--- Rustlang/src/main.rs
+++ Rustlang/src/main.rs
@@ -1,3 +1,36 @@
+use std::error::Error;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn calculate() -> f32 {
+    let mut accumulator: f32 = 0.0;
+
+    while true {
+        match query_next_operation() {
+            Ok(op) => {
+                match do_operation(op, accumulator) {
+                    Ok(result) => accumulator = result,
+                    Err(e) => {
+                        
+                    },
+                }
+            },
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+
+    accumulator
+}
+
+fn query_next_operation() -> Result<Operation, Error> {
+    println!("Enter operator and an optional operand: ");
+
+
+}
+
+struct Operation {
+    operator: char,
+    operand: f32,
 }
