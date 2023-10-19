901c80cd4014bd8ca1995c02cc28ca5b67283998
Kresten Laust <krestenlaust@gmail.com>
2023-10-19 13:32:04 +0200

    Added first level topdown

--- src/project1.c
+++ src/project1.c
@@ -1,5 +1,15 @@
 #include "calculator.h"
 
 int main(void) {
+    // declare mutable variables
 
+    // - loop begin
+
+    // inform user of calculator state / output accumulator
+    // retrieve operation information (read and parse user input)
+    // pass operation to calculator
+
+    // - loop end
+
+    // finalize / output result
 }
