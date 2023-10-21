c8f540545053875115beef0797f99f93744d0b71
Kresten Laust <krestenlaust@gmail.com>
2023-10-21 13:00:14 +0200

    Made use of symbols

--- src/calculator.c
+++ src/calculator.c
@@ -5,11 +5,23 @@ void do_next_op(char operator, double operand, double* akkumulator) {
 }
 
 bool is_op_valid(char operator) {
-
+    return operator == PLUS_OP ||
+        operator == MINUS_OP ||
+        operator == MULTIPLICATION_OP ||
+        operator == DIVISION_OP ||
+        operator == POWER_OP ||
+        operator == SQUAREROOT_OP ||
+        operator == FLIP_SIGN_OP ||
+        operator == RECIPROCAL_OP ||
+        operator == EXIT_OP;
 }
 
 bool is_op_binary(char operator) {
-    return operator == '+' || operator == '-' || operator == '*' || operator == '/' || operator == '^';
+    return operator == PLUS_OP ||
+        operator == MINUS_OP ||
+        operator == MULTIPLICATION_OP ||
+        operator == DIVISION_OP ||
+        operator == POWER_OP;
 }
 
 binary_operator binary_op_factory(char operator){
