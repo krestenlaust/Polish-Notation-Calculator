1de6df57b66419d66bf4e8bfd63198d77dfa3097
Kresten Laust <krestenlaust@gmail.com>
2023-10-23 14:37:28 +0200

    Added operator signatures

--- src/calculator.c
+++ src/calculator.c
@@ -1,7 +1,23 @@
 #include "calculator.h"
+#include "concrete_operators.h"
+#include <stdio.h>
+
+bool is_op_binary(char operator);
+unary_operator unary_op_factory(char operator);
+binary_operator binary_op_factory(char operator);
 
 void do_next_op(char operator, double operand, double* accumulator) {
+    double result;
 
+    if (is_op_binary(operator)){
+        binary_operator op_func = binary_op_factory(operator);
+        result = op_func(*accumulator, operand);
+    }else{
+        unary_operator op_func = unary_op_factory(operator);
+        result = op_func(*accumulator);
+    }
+
+    *accumulator = result;
 }
 
 bool is_op_valid(char operator) {
@@ -25,13 +41,33 @@ bool is_op_binary(char operator) {
 }
 
 binary_operator binary_op_factory(char operator){
+    switch (operator) {
+        case PLUS_OP:
+            return add;
+        case MINUS_OP:
+            return subtract;
+        case MULTIPLICATION_OP:
+            return multiply;
+        case DIVISION_OP:
+            return divide;
+        case POWER_OP:
+            return power;
+    }
 
+    return NULL;
 }
 
 unary_operator unary_op_factory(char operator) {
+    switch (operator) {
+        case SQUAREROOT_OP:
+            return squareroot;
+        case NEGATE_OP:
+            return negate;
+        case RECIPROCAL_OP:
+            return reciprocal;
+        case EXIT_OP:
+            return quit;
+    }
 
-}
-
-unary_operator partial_binary_op(binary_operator op, double second_operand) {
-    
+    return NULL;
 }
---
+++ src/concrete_operators.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <stdbool.h>
+
+
+double add(double x, double y);
+double subtract(double x, double y);
+double multiply(double x, double y);
+double divide(double x, double y);
+double power(double x, double y);
+
+double squareroot(double x);
+double negate(double x);
+double reciprocal(double x);
+double quit(double x);
