e8ed8c1403ed10a6c6d5028521ba5341e7eccb31
Kresten Laust <krestenlaust@gmail.com>
2023-10-23 18:54:27 +0200

    Added input signatures and structure

--- src/project1.c
+++ src/project1.c
@@ -1,24 +1,33 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
 #include "calculator.h"
+#include "input_reader.h"
+
+double run_calculator();
 
 int main(void) {
-    run_calculator();
+    double result = run_calculator();
 }
 
-double run_calculator(){
+double run_calculator(void){
     // declare mutable variables
     double accumulator = 0.0;
 
-    // - loop begin
     while (true) {
-        printf("Enter operator, and an optional operand: ");
+        printf("\nEnter operator, and an optional operand: ");
+
         // retrieve operation information (read and parse user input)
+        operation op = scan_data();
+
         // pass operation to do_next_op and return result
+        do_next_op(op.operator, op.operand, &accumulator);
+
         // inform user of calculator state / output accumulator
+        printf("\nResult so far is %lf", accumulator);
     }
 
-    
-
-    // - loop end
-
-    // finalize / output result
+    // Never reached, since quit is unary.
+    return accumulator;
 }
